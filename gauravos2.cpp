147bb19ea5f8780913b8a19848ba805fa1ac03a3
thegauravgupta <38029204+thegauravgupta@users.noreply.github.com>
2018-04-13 04:27:22 +0530

    Add files via upload

--- gauravos2.cpp
+++ gauravos2.cpp
@@ -3,34 +3,43 @@
 using namespace std;
 main()
 {
-char name[4]={'p1','p2','p3','p4'};
+char name[4]={'a','b','c','d'},sq[15];
 int ar[4]={0,2,4,13};
 int bu[4]={18,23,13,10},time=0;
+cout<<" time interval = 3 \n";
 	for(int i=0;i<4;i++)
 	{
 		if(ar[i]<=time && bu[i]>0)
 		{
+			time=time+3;
 			bu[i]=bu[i]-3;
 			cout<<name[i];
-			time=time+3;
+			
 		}
 	}
+	
+cout<<"\ntime interval =6\n";
 	for(int i=0;i<4;i++)
 	{
 		if(ar[i]<=time && bu[i]>0)
 		{
+			time=time+6;
 			bu[i]=bu[i]-6;
 			cout<<name[i];
-			time=time+6;
+			
 		}
 	}
+	cout<<"\nrest of the code first come first serve \n";
 	for(int i=0;i<4;i++)
 	{
 		if(ar[i]<=time && bu[i]>0)
 		{
+			time=time+bu[i];
 			bu[i]=bu[i]-bu[i];
 			cout<<name[i];
-			time=time+bu[i];
+			
 		}
+		
 	}
+	cout<<"\n total time consumed is :"<<time<<"\n";
 }
