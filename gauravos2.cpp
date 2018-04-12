724a8890822a5a1af0de16a76fb76958e9727eb8
thegauravgupta <38029204+thegauravgupta@users.noreply.github.com>
2018-04-13 05:22:48 +0530

    Add files via upload

--- gauravos2.cpp
+++ gauravos2.cpp
@@ -3,19 +3,22 @@
 using namespace std;
 main()
 {
-char name[4]={'a','b','c','d'},sq[15];
-int ar[4]={0,2,4,13},j=0;
-int bu[4]={18,23,13,10},time=0;
-cout<<" time interval = 3 \n";
+char name[4]={'a','b','c','d'},sequence[15];
+int arrival_time[4]={0,2,4,13},j=0;
+int burst_time[4]={18,23,13,10},time=0;
+cout<<"\n process 1 - a - arraival time : 0  - burst time : 18\n process 2 - b - arraival time : 2  - burst time : 23 \n process 3 - c - arraival time : 4  - burst time : 13\n process 3 - d - arraival time : 13 - burst time : 10\n";
+
+
+cout<<"\n time interval = 3 \n";
 cout<<"\n sequence \n";
 	for(int i=0;i<4;i++)
 	{
-		if(ar[i]<=time && bu[i]>0)
+		if(arrival_time[i]<=time && burst_time[i]>0)
 		{   cout<<" ";
 			time=time+3;
-			bu[i]=bu[i]-3;
+			burst_time[i]=burst_time[i]-3;
 			cout<<name[i];
-			sq[j]=name[i];
+			sequence[j]=name[i];
 			j++;
 		}
 	}
@@ -24,13 +27,13 @@ cout<<"\n\n time interval =6\n";
 cout<<"\n sequence \n";
 	for(int i=0;i<4;i++)
 	{
-		if(ar[i]<=time && bu[i]>0)
+		if(arrival_time[i]<=time && burst_time[i]>0)
 		{
 			cout<<" ";
 			time=time+6;
-			bu[i]=bu[i]-6;
+			burst_time[i]=burst_time[i]-6;
 			cout<<name[i];
-			sq[j]=name[i];
+			sequence[j]=name[i];
 			j++;
 		}
 	}
@@ -38,23 +41,23 @@ cout<<"\n sequence \n";
 	cout<<"\n sequence \n";
 	for(int i=0;i<4;i++)
 	{
-		if(ar[i]<=time && bu[i]>0)
+		if(arrival_time[i]<=time && burst_time[i]>0)
 		{   cout<<" ";
-			time=time+bu[i];
-			bu[i]=bu[i]-bu[i];
+			time=time+burst_time[i];
+			burst_time[i]=burst_time[i]-burst_time[i];
 			cout<<name[i];
-			sq[j]=name[i];
+			sequence[j]=name[i];
 			j++;
 		}
 		
 	}
 	cout<<"\n total time consumed is :"<<time<<"\n";
-	cout<<"\n sequence \n";
+	cout<<"\nfinal sequence \n";
 	cout<<"--------------------------";
 	cout<<"\n";
 	for(int y=0;y<j;y++)
 	{
-		cout<<"|"<<sq[y];
+		cout<<"|"<<sequence[y];
 	}
 	cout<<"\n \nend of the program";
 }
