a411fabcbe192ff2ba61bcf972408a9724bcd95d
thegauravgupta <38029204+thegauravgupta@users.noreply.github.com>
2018-04-13 04:37:40 +0530

    Add files via upload

--- gauravos2.cpp
+++ gauravos2.cpp
@@ -7,10 +7,11 @@ char name[4]={'a','b','c','d'},sq[15];
 int ar[4]={0,2,4,13},j=0;
 int bu[4]={18,23,13,10},time=0;
 cout<<" time interval = 3 \n";
+cout<<"\n sequence \n";
 	for(int i=0;i<4;i++)
 	{
 		if(ar[i]<=time && bu[i]>0)
-		{
+		{   cout<<" ";
 			time=time+3;
 			bu[i]=bu[i]-3;
 			cout<<name[i];
@@ -19,11 +20,13 @@ cout<<" time interval = 3 \n";
 		}
 	}
 	
-cout<<"\ntime interval =6\n";
+cout<<"\n\n time interval =6\n";
+cout<<"\n sequence \n";
 	for(int i=0;i<4;i++)
 	{
 		if(ar[i]<=time && bu[i]>0)
 		{
+			cout<<" ";
 			time=time+6;
 			bu[i]=bu[i]-6;
 			cout<<name[i];
@@ -31,11 +34,12 @@ cout<<"\ntime interval =6\n";
 			j++;
 		}
 	}
-	cout<<"\nrest of the code first come first serve \n";
+	cout<<"\n\n rest of the code first come first serve \n";
+	cout<<"\n sequence \n";
 	for(int i=0;i<4;i++)
 	{
 		if(ar[i]<=time && bu[i]>0)
-		{
+		{   cout<<" ";
 			time=time+bu[i];
 			bu[i]=bu[i]-bu[i];
 			cout<<name[i];
@@ -52,4 +56,5 @@ cout<<"\ntime interval =6\n";
 	{
 		cout<<"|"<<sq[y];
 	}
+	cout<<"\n \nend of the program";
 }
